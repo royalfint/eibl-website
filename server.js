45a3b011443f387e9cef5873435ca0497c6c14d8
Vlad <royalfint@hotmail.com>
2017-08-28 16:30:27 +0600

    almost heroku ready

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-// server.js
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/src'));
-// Start the app by listening on the default
-// Heroku port
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 8080);
+
+// PathLocationStatagy
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+console.log("COnsole listening!");
