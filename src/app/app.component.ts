bf8867521ec2363b4e1276537c3569eef8fdd6be
Vlad <royalfint@hotmail.com>
2017-08-28 17:49:15 +0600

    loading animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None,
-  animations: [
-    trigger('loadingAnim', [
-      state('loading', style({
-        height: "0vh",
-      })),
-      state('loaded', style({
-        height: "100vh",
-      })),
-      transition('loading <=> loaded', animate('3000ms ease-out')),
-    ])
-  ]
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app';
-  loading = "loading";
-
-  load() {
-    this.loading = "loaded";   
-  }
-
-  constructor(){
-    setTimeout(()=>{
-      this.load();
-    },1000);
-  }
 }
